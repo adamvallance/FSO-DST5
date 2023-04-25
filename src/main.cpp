43095e1a87c082b94a3c39dddad632e152ac1f78
Adam Vallance <adamvallance18@gmail.com>
2023-04-25 14:40:43 +0100

    Started building a controller class, the main class, to be initialised in main. Just blinks for now

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,20 @@
  */
 
 #include "mbed.h"
-#include "config.h"
+#include "FSOcontroller.h"
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     100ms
+// // Blinking rate in milliseconds
+// #define BLINKING_RATE     100ms
 
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(PIN_FRDM_LED_GREEN);
-
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+    FSOcontroller();
+//    // Initialise the digital pin LED1 as an output
+//    DigitalOut led(PIN_FRDM_LED_GREEN);
+//
+//     while (true) {
+//         led = !led;
+//         ThisThread::sleep_for(BLINKING_RATE);
+//     }
 }
