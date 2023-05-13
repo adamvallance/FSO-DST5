9d4499ad78f34eba361e252e38c0a27a69a4f6a1
Adam Vallance <adamvallance18@gmail.com>
2023-05-13 18:02:28 +0100

    following failed reads for sfp not there it returns 0 now

--- src/sfp.cpp
+++ src/sfp.cpp
@@ -30,11 +30,17 @@ float SFP::getRXPower(){
     }
     else if (SFPindex <5){
         I2CB.write(SFP_I2C_ADDRESS, &SFP_RX_POWER_REG_ADDRESS, 1);
-        I2CB.read(SFP_I2C_ADDRESS, powerRec, 2);
+        if (I2CB.read(SFP_I2C_ADDRESS, powerRec, 2) !=0){ //if read fails - if sfp not installed
+            return 0.0;
+        }
+        
     }else{
         I2CA.write(SFP_I2C_ADDRESS, &SFP_RX_POWER_REG_ADDRESS, 1);
-        I2CA.read(SFP_I2C_ADDRESS, powerRec, 2);
-    }
+        if (I2CA.read(SFP_I2C_ADDRESS, powerRec, 2) != 0){ //if read fails return 0
+            return 0.0;
+        }
+
+    } 
     powerInt = (powerRec[0] << 8) + powerRec[1]; //unsigned int
 
     
